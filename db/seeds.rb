001b894640aa01a878ad5938d605a8fe4bb2bf87
Hayden Brown <hbrown10@binghamton.edu>
2019-05-15 15:27:27 +0000

    Add sample food to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 Restaurant.create(name: "Nirchi's", address: "3619 Vestal Parkway, Vestal, NY 13850", phone_number: "607-729-5131", description: "We serve pizza and italian cuisine", user_id: 1)
 Restaurant.create(name: "Number 5", address: "33 South Washington St. Binghamton, NY 13903", phone_number: "607-723-0555", description: "Prime Steaks, Fresh Seafood Since 1978", user_id: 1)
 Restaurant.create(name: "Lost Dog Cafe", address: "222 Water Street, Binghamton, NY 13901", phone_number: "607-771-6063", description: "Serves lunch, dinner, coffee and drinks", user_id: 2)
+Food.create(name: "Pasta", price: "5", tags: "italian;pasta;sauce;delicious", restaurant_id: 3, rating: 0, number_of_ratings: 0)
