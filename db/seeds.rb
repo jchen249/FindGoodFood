fdd7e6df84c7a456bfa8cf112de4ed0361ea1535
Hayden Brown <hbrown10@binghamton.edu>
2019-05-15 13:47:27 +0000

    add some seeds for db generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(name: "Nirchi's", address: "3619 Vestal Parkway, Vestal, NY 13850", phone_number: "607-729-5131", description: "We serve pizza and italian cuisine", user_id: 1)
+Restaurant.create(name: "Number 5", address: "33 South Washington St. Binghamton, NY 13903", phone_number: "607-723-0555", description: "Prime Steaks, Fresh Seafood Since 1978", user_id: 1)
+Restaurant.create(name: "Lost Dog Cafe", address: "222 Water Street, Binghamton, NY 13901", phone_number: "607-771-6063", description: "Serves lunch, dinner, coffee and drinks", user_id: 2)
