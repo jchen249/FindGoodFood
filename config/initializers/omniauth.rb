57e599d86a035b2bb296cdaf5292eb38d2c1b953
Hayden Brown <hbrown10@binghamton.edu>
2019-05-19 17:01:52 +0000

    commented out line 9

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     
     
     if Rails.env.production?
-        provider :production if Rails.env.production? 
+        # provider :production if Rails.env.production? 
         provider :facebook , '1438381289635819', 'dccd0d31a98e1361e4ae69c6bf01f0c2'
     else
         provider :developer if Rails.env.development?
