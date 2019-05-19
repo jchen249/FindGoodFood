c9d8b913b168df274ed4e524796603b392ea67d5
Hayden Brown <hbrown10@binghamton.edu>
2019-05-19 17:00:24 +0000

    fixed typo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     
     if Rails.env.production?
         provider :production if Rails.env.production? 
-        provider: facebook , '1438381289635819', 'dccd0d31a98e1361e4ae69c6bf01f0c2'
+        provider :facebook , '1438381289635819', 'dccd0d31a98e1361e4ae69c6bf01f0c2'
     else
         provider :developer if Rails.env.development?
         provider :github, '2060b14d94c254653210', '36d8c492e7905333ab5894c5bddb34c0081b4b50',
